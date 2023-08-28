9bda55bad4a2889cea8ef65cd930d430c7e0ccd3
Muhammad Faizan <143316927+MuhammadFaizan9200@users.noreply.github.com>
2023-08-28 07:04:19 -0700

    Push all files

---
+++ database/migrations/2023_03_12_020058_create_memberships_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembershipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('persons')->nullable();
+            $table->timestamp('date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('memberships', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('email');
+            $table->dropColumn('phone_number');
+            $table->dropColumn('persons');
+            $table->dropColumn('date');
+        });
+    }
+}
